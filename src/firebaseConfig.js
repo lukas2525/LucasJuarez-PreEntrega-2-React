4ccb074397c3bf27f6cb599eec03fbac07bc4e83
Lucas Juarez <juarezlucasangel@gmail.com>
2023-04-03 16:40:09 -0300

    nuevas actualizaciones

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY ,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
-  projectId: process.env.REACT_APP_PROJECT_ID ,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyC6U7-ruNvtEzwKbZX6v8zNpsxDRpq_FBo",
+  authDomain: "primer-proyecto-react-38a61.firebaseapp.com",
+  projectId: "primer-proyecto-react-38a61",
+  storageBucket: "primer-proyecto-react-38a61.appspot.com",
+  messagingSenderId: "834384135322",
+  appId: "1:834384135322:web:5ba5c46333bf15eb12cb23"
 };
 
 const app = initializeApp(firebaseConfig);
